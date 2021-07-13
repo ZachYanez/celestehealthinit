267293ff2134b7bd90e8977026e76b9606d349a7
Zach Yanez <zachyanez@gmail.com>
2021-07-13 14:07:00 -0500

    initial commit

---
+++ src/pages/WeAre.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function WeAre() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Who We Are Page</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Who We Are Page
+        </a>
+      </header>
+    </div>
+  );
+}
